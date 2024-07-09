98c2451a4203d6e67eaf55447fdd76c9552c1d6f
Matilda Horger <matilda.horger@gmail.com>
2024-07-09 23:05:50 +0000

    Put all non-secret-loading in one profile script

--- builders/dotfiles/deb/etc/profile.d/mm4cc-onlogin.sh
+++ builders/dotfiles/deb/etc/profile.d/mm4cc-onlogin.sh
@@ -1,7 +1,11 @@
+# Set up PATH with extra entries
 paths=("$HOME/bin" "$HOME/.cargo/bin")
-
 for path in ${paths[@]}; do
   if [ -e "$path" ]; then
     export PATH="$PATH:$path"
   fi
 done
+
+if (which ssh-agent > /dev/null); then
+  eval $(ssh-agent)
+fi
