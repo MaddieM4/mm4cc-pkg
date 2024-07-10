040bf807224cade73ae5f39cecaf4a30709be778
Matilda Horger <matilda.horger@gmail.com>
2024-07-10 00:18:40 +0000

    Only package dotfiles - don't develop tooling here

--- builders/dotfiles/deb/etc/profile.d/mm4cc-secrets.sh
+++
@@ -1,7 +0,0 @@
-if [ -e $HOME/.config/mm4cc/secrets ]; then
-  pushd $HOME/.config/mm4cc/secrets > /dev/null
-  for name in *; do
-    export "$name"="$(cat "$name")"
-  done
-  popd > /dev/null
-fi
